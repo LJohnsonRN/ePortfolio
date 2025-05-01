ebd14080fbcdb454847281f919c009270b194bc7
Seth <SJalltheway5@aol.com>
2025-05-01 11:36:52 -0700

    Hamburger menu: size limits and rotation

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+let rotation = 0;
 document.getElementById('menu-toggle').addEventListener('click', function() {
   const menu = document.getElementById('side-menu');
   menu.classList.toggle('show');
+  if (rotation === 0) {
+    rotation = -180;
+  } else {
+    rotation = 0;
+  }
+  this.style.transform = `rotate(${rotation}deg)`;
 });
 
 const menuItems = document.querySelectorAll('.menu-item');
